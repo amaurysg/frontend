1cf9fd19f19077cfc6676085ca8aa235c24488dc
Amaury Soto <amaurysotog@gmail.com>
2020-05-21 14:30:27 -0500

    Version 1 Frontend + Reto Sesión

---
+++ repaso.js
@@ -0,0 +1,8 @@
+function mostrarNum() {
+  var num1 = 25;
+  for (var num1 = 25; num1 < 150; num1 * 2) {
+    console.log(num1);
+  }
+}
+
+mostrarNum();
---
+++ repaso1.js
@@ -0,0 +1,23 @@
+function numeros5() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i * 25);
+  }
+}
+
+numeros5();
+
+var i = 0;
+result = "";
+while (result < 100) {
+  result = i++ * 5;
+  console.log(result);
+}
+
+let thora = 3300;
+
+function pago(hr) {
+  pago = hr * thora;
+  console.log(pago);
+}
+
+pago(14);
